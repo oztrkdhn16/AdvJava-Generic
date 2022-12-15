e88a5ddaecd56aa6e78535d875bed45d7dde488c
Duhan Ozturk <duhan9885@gmail.com>
2022-12-15 22:23:37 +0300

    Student Proje

---
+++ classes/Student.java
@@ -0,0 +1,44 @@
+package AdvJavaPractice.generics.classes;
+
+
+// 3 Parametreli generic sinif tanimlayiniz.
+
+
+public class Student<S,U,V> {
+
+    private S s;
+    private U u;
+    private V v;
+
+    public Student(S s, U u, V v) {
+        this.s = s;
+        this.u = u;
+        this.v = v;
+    }
+
+    public S getS() {
+        return s;
+    }
+
+    public void setS(S s) {
+        this.s = s;
+    }
+
+    public U getU() {
+        return u;
+    }
+
+    public void setU(U u) {
+        this.u = u;
+    }
+
+    public V getV() {
+        return v;
+    }
+
+    public void setV(V v) {
+        this.v = v;
+    }
+
+
+}
